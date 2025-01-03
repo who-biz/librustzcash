dd1ee88d81e6403562a2add915b3b313dbe9d6a0
who-biz <37732338+who-biz@users.noreply.github.com>
2025-01-03 14:48:38 -0500

    Add breakpoints to analyze sapling tree population

--- zcash_client_sqlite/src/lib.rs
+++ zcash_client_sqlite/src/lib.rs
@@ -818,10 +818,11 @@ impl<P: consensus::Parameters> WalletWrite for WalletDb<rusqlite::Connection, P>
             // `blocks` is non-empty.
 
             //Note: newly added by Biz
-            #[cfg(feature = "orchard")]
+            //#[cfg(feature = "orchard")]
             if let Some((start_positions, last_scanned_height)) =
                 start_positions.zip(last_scanned_height)
             {
+                warn!(">>> bp1 sapling tree");
                 // Create subtrees from the note commitments in parallel.
                 const CHUNK_SIZE: usize = 1024;
                 let sapling_subtrees = sapling_commitments
@@ -859,6 +860,7 @@ impl<P: consensus::Parameters> WalletWrite for WalletDb<rusqlite::Connection, P>
                     .map(|res| (res.subtree, res.checkpoints))
                     .collect::<Vec<_>>();
 
+                warn!(">>> bp2 sapling tree");
                 // Collect the complete set of Sapling checkpoints
 		// TODO: check if this sapling-based code should actually be behind this
 		// conditional compilation cfg macro
@@ -943,11 +945,15 @@ impl<P: consensus::Parameters> WalletWrite for WalletDb<rusqlite::Connection, P>
                     ),
                 );
 
+                //TODO: newly added by Biz, need to add a pre-compile feature to disable relevant subtrees
+                #[cfg(feature = "orchard")]
                 // Update the Sapling note commitment tree with all newly read note commitments
                 {
+                    warn!(">>> bp3 sapling tree");
+
                     let mut sapling_subtrees_iter = sapling_subtrees.into_iter();
                     wdb.with_sapling_tree_mut::<_, _, Self::Error>(|sapling_tree| {
-                        debug!(
+                        warn!(
                             "Sapling initial tree size at {:?}: {:?}",
                             from_state.block_height(),
                             from_state.final_sapling_tree().tree_size()
@@ -1054,6 +1060,7 @@ impl<P: consensus::Parameters> WalletWrite for WalletDb<rusqlite::Connection, P>
                 )?;
             }
 
+            warn!(">>> bp4 sapling tree");
             Ok(())
         })
     }
