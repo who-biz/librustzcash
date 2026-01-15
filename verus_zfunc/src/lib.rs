d45c958e77db51086b10d86648bc549a6c308709
who-biz <37732338+who-biz@users.noreply.github.com>
2026-01-15 16:44:46 -0500

    Revert some changes to debug regression

--- verus_zfunc/src/lib.rs
+++ verus_zfunc/src/lib.rs
@@ -300,20 +300,20 @@ pub fn z_getencryptionaddress(params: RpcParams) -> Result<ChannelKeys> {
     }
 
     // here is our unique, deterministic seed for the communication channel
-    let channel_seed = Secret::<[u8; 32]>::new(Sha256::digest(&encryption_seed_bytes).into());
+    let channel_seed: [u8; 32] = Sha256::digest(&encryption_seed_bytes).into();
 
 
     //TODO: (Biz) we can almost certainly get rid of all these intermediate variables below,
     // we are only using the last variable in this anyway, the rest are meaningless and incorrect
 
-    /*let channel_master_sk = ExtendedSpendingKey::master(channel_seed.expose_secret());
+    let channel_master_sk = ExtendedSpendingKey::master(&channel_seed);
     let channel_purpose = channel_master_sk.derive_child(ChildIndex::hardened(32));
     let channel_coin = channel_purpose.derive_child(ChildIndex::hardened(VERUS_COIN_TYPE)); // use same coin type as above
-    let final_sk = channel_coin.derive_child(ChildIndex::hardened(params.encryption_index));*/
+    let final_sk = channel_coin.derive_child(ChildIndex::hardened(params.encryption_index));
 
     // use the new channel seed to derive the final key for this channel
     // use caller-provided encryption index, with path (m/32'/coin_type'/encryption_index')
-    let channel_sk = ExtendedSpendingKey::master(channel_seed.expose_secret()).derive_child(ChildIndex::hardened(params.encryption_index));
+    //let channel_sk = ExtendedSpendingKey::master(channel_seed.expose_secret()).derive_child(ChildIndex::hardened(params.encryption_index));
     //channel_master_sk.derive_child(ChildIndex::hardened(32));
     //channel_master_sk.derive_child(ChildIndex::hardened(VERUS_COIN_TYPE));
     //channel_master_sk.derive_child(ChildIndex::hardened(params.encryption_index));
@@ -321,14 +321,14 @@ pub fn z_getencryptionaddress(params: RpcParams) -> Result<ChannelKeys> {
 
     // (Biz) Didn't think we needed this one below, but looks like we do. this is a extended diversifiable fvk
     // it includes *all* information present in a dfvk and and extfvk
-    let extended_dfvk = key_encoding::serialize_extended_dfvk(&channel_sk.to_extended_full_viewing_key());
+    let extended_dfvk = key_encoding::serialize_extended_dfvk(&final_sk.to_extended_full_viewing_key());
 
     // bech32 encode it
     //let fvk_bech = key_encoding::encode_xfvk(&xfvk)?;
     
     // (Biz) dfvk includes all of the information we need, xfvk above was redundant
     // We should also convert to SecretVec as soon as possible, always for secret info
-    let dfvk = channel_sk.to_diversifiable_full_viewing_key();
+    let dfvk = final_sk.to_diversifiable_full_viewing_key();
 
     //let network = Network::MainNetwork;
     let (_/*diversifier*/, payment_address) = dfvk.default_address();
@@ -345,7 +345,7 @@ pub fn z_getencryptionaddress(params: RpcParams) -> Result<ChannelKeys> {
         fvk_bytes: extended_dfvk,
         dfvk_bytes: SecretVec::new(dfvk.to_bytes().into()),
         spending_key_bytes: if params.return_secret {
-            Some(SecretVec::new(channel_sk.to_bytes().into())) 
+            Some(SecretVec::new(final_sk.to_bytes().into())) 
         } else {
             None
         },
